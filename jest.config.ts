1108dccef4893fa69fdbb3ee81b05a50d931c004
Gustavo <gustavo@bgcbrasil.com.br>
2023-05-28 15:46:25 -0300

    test: configure tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,16 +4,28 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest-env.ts'],
-  collectCoverageFrom: ['<rootDir>/src/infrastructure/controllers/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/infrastructure/**/*.ts',
+    '!<rootDir>/src/infrastructure/**/bodySchema.ts',
+    '<rootDir>/src/infrastructure/repositories/*.ts',
+    '!<rootDir>/src/app/repositories/*.ts',
+    '<rootDir>/src/app/entities/*.ts',
+    '<rootDir>/src/app/useCases/**/*.ts',
+    '<rootDir>/src/main/**/*.ts',
+    '!<rootDir>/src/main/config/**/*.ts',
+    '!<rootDir>/src/main/factories/**/*.ts',
+    '!<rootDir>/src/main/routes/**/*.ts',
+    '!<rootDir>/src/main/server.ts',
+  ],
   moduleNameMapper: {},
   transform: {
     '^.+\\.(ts|tsx)?$': '@swc/jest',
   },
   clearMocks: true,
-  // coverageThreshold: {
-  //   global: {
-  //     lines: 100,
-  //   },
-  // },
+  coverageThreshold: {
+    global: {
+      lines: 100,
+    },
+  },
 };
 export default config;
