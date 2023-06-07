c957a68f33f1acdd38f76e3ddffb4e18786aed35
Gustavo <gustavo@bgcbrasil.com.br>
2023-06-06 21:30:34 -0300

    feat: create Dog Entity

---
+++ src/app/entities/dog.ts
@@ -0,0 +1,87 @@
+import { v4 as uuidV4 } from 'uuid';
+import type { DogSizeEnum, DogTemperamentEnum } from '../enums';
+
+interface IDog<DateType> {
+  id: string;
+  name: string;
+  breed: string;
+  size: DogSizeEnum;
+  birthDate: DateType;
+  temperament: DogTemperamentEnum;
+  createdAt: DateType;
+  deletedAt?: DateType;
+  tutorId: string;
+}
+
+export interface IOptions {
+  id?: string;
+  createdAt?: Date;
+  deletedAt?: Date;
+}
+
+export class Dog {
+  private props: IDog<Date>;
+
+  constructor(props: Omit<IDog<Date>, 'id'>, options: IOptions = {}) {
+    this.props = {
+      id: options.id || uuidV4(),
+      createdAt: options.createdAt || new Date(),
+      deletedAt: options.deletedAt,
+      name: props.name,
+      breed: props.breed,
+      size: props.size,
+      birthDate: props.birthDate,
+      temperament: props.temperament,
+      tutorId: props.tutorId,
+    };
+  }
+
+  get id(): string {
+    return this.props.id;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get breed(): string {
+    return this.props.breed;
+  }
+
+  get size(): DogSizeEnum {
+    return this.props.size;
+  }
+
+  get birthDate(): Date {
+    return this.props.birthDate;
+  }
+
+  get temperament(): DogTemperamentEnum {
+    return this.props.temperament;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get deletedAt(): Date | undefined {
+    return this.props.deletedAt;
+  }
+
+  get tutorId(): string {
+    return this.props.tutorId;
+  }
+
+  public delete(): void {
+    this.props.deletedAt = new Date();
+  }
+
+  public toJSON(): IDog<string> {
+    return {
+      ...this.props,
+      birthDate: this.props.birthDate.toISOString(),
+      createdAt: this.props.createdAt.toISOString(),
+      deletedAt: this.props.deletedAt?.toISOString(),
+    };
+  }
+}
---
+++ src/app/enums/index.ts
@@ -0,0 +1,12 @@
+export enum DogSizeEnum {
+  TINY = 'TINY',
+  SMALL = 'SMALL',
+  MEDIUM = 'MEDIUM',
+  BIG = 'BIG',
+}
+
+export enum DogTemperamentEnum {
+  COOL = 'COOL',
+  SHY = 'SHY',
+  ANGRY = 'ANGRY',
+}
