37942b3d8f2733f616129147879f620e195baa3b
Gustavo <gustavo@bgcbrasil.com.br>
2023-06-11 13:06:13 -0300

    feat: create CreateUserController

---
+++ src/app/controllers/createUser.controller.ts
@@ -0,0 +1,79 @@
+import { UserRoleEnum } from '../enums';
+import { CreateUserUseCase } from '../useCases/createUser.useCase';
+import type { IController, IRequest, IResponse } from './IController';
+
+interface IBody {
+  name: string;
+  email: string;
+  cpf: string;
+  telephone: string;
+  password: string;
+  role: UserRoleEnum;
+}
+
+export class CreateUserController implements IController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  async handle(request: IRequest): Promise<IResponse> {
+    const { name, email, cpf, telephone, password, role } = request.body as IBody;
+
+    if (!name || !email || !cpf || !telephone || !password || !role) {
+      return {
+        statusCode: 400,
+        body: {
+          message: 'Missing parameters',
+        },
+      };
+    }
+
+    if (
+      role !== UserRoleEnum.TUTOR &&
+      role !== UserRoleEnum.WALKER &&
+      role !== UserRoleEnum.ADMIN
+    ) {
+      return {
+        statusCode: 400,
+        body: {
+          message: 'Invalid role',
+        },
+      };
+    }
+
+    try {
+      const user = await this.createUserUseCase.execute({
+        name,
+        email,
+        cpf,
+        telephone,
+        password,
+        role,
+      });
+
+      return {
+        statusCode: 200,
+        body: {
+          message: 'User created successfully',
+          user: user.toJSON(),
+        },
+      };
+    } catch (error) {
+      console.log({ error });
+
+      if ((error as Error).message === 'Email already in use') {
+        return {
+          statusCode: 409,
+          body: {
+            message: 'Email already in use',
+          },
+        };
+      }
+
+      return {
+        statusCode: 500,
+        body: {
+          message: 'Internal Server Error',
+        },
+      };
+    }
+  }
+}
