f7ae36aecf4e8cc0cecdbb2686c8ed74e7e6f96d
Gustavo <gustavo@bgcbrasil.com.br>
2023-06-14 20:56:26 -0300

    test: getTripsUseCase 100% coverage

---
+++ tests/app/useCases/getTripsUseCase.test.ts
@@ -0,0 +1,28 @@
+import { GetTripsUseCase } from '../../../src/app/useCases/getTrips.useCase';
+import { MockTripRepository } from '../../../src/infrastructure/repositories/mock/trip.repository';
+
+describe('GetTripsUseCase', () => {
+  let getTripsUseCase: GetTripsUseCase;
+
+  beforeEach(() => {
+    getTripsUseCase = new GetTripsUseCase(new MockTripRepository());
+  });
+
+  it('should return an array of trips of walkerId', async () => {
+    const trips = await getTripsUseCase.execute({ date: new Date(), walkerId: '1' });
+
+    expect(trips.length).toBe(4);
+  });
+
+  it('should return an empty array when missing date', async () => {
+    const trips = await getTripsUseCase.execute({} as any);
+
+    expect(trips.length).toBe(0);
+  });
+
+  it('should return an array of trips of zipCode', async () => {
+    const trips = await getTripsUseCase.execute({ date: new Date(), zipCode: '1' });
+
+    expect(trips.length).toBe(4);
+  });
+});
