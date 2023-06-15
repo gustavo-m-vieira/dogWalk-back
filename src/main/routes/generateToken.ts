e8441e66a4a2762cabc85240c09af948479f36ea
Gustavo <gustavo@bgcbrasil.com.br>
2023-06-15 11:41:51 -0300

    fix: change Generate Token now its POST

--- src/main/routes/generateToken.ts
+++ src/main/routes/generateToken.ts
@@ -3,5 +3,5 @@ import { makeAuthenticateController } from '../factories';
 import { adaptRoute } from '../adapters/adaptRoute';
 
 export default (router: Router): void => {
-  router.get('/generate-token', adaptRoute(makeAuthenticateController()));
+  router.post('/generate-token', adaptRoute(makeAuthenticateController()));
 };
